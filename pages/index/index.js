697ecd4916a5d805427db54caa8236996eb2fbb2
joe <2508387422@qq.com>
2018-12-03 19:46:39 +0800

    首页替换为data属性

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,132 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     archivesList: [1],
-    lessonList: [1, 2, 3]
+    
+    // 用户课程
+    userLessons: [{
+        "id": "11",
+        "courseName": "狗狗学做饭-1",
+        "courseTitle": "做饭",
+        "courseLengthTime": "15",
+        "courseType": "2",
+        "courseDesc": "狗狗学做饭",
+        "courseLabel": "做饭",
+        "courseCost": "20",
+        "courseImage": "FASLAJSDIUQWHDKJASHDKJASDASD",
+        "saleState": "1",
+        "courseOrder": "1000",
+        "trainerInfo": "范辉",
+        "level": "3",
+        "learningStep": "不知道",
+        "learningTimes": "11",
+        "JoinerCount": "1100",
+        "creatTime": "2018-06-15",
+        "creater": "范辉"
+      },
+      {
+        "id": "12",
+        "courseName": "狗狗学做饭-2",
+        "courseTitle": "做饭",
+        "courseLengthTime": "10",
+        "courseType": "2",
+        "courseDesc": "狗狗学做饭",
+        "courseLabel": "做饭",
+        "courseCost": "20",
+        "courseImage": "FASLAJSDIUQWHDKJASHDKJASDASD",
+        "saleState": "1",
+        "courseOrder": "1000",
+        "trainerInfo": "范辉",
+        "level": "3",
+        "learningStep": "不知道",
+        "learningTimes": "11",
+        "JoinerCount": "1100",
+        "creatTime": "2018-06-15",
+        "creater": "范辉"
+      },
+      {
+        "id": "13",
+        "courseName": "狗狗学做饭-3",
+        "courseTitle": "做饭",
+        "courseLengthTime": "20",
+        "courseType": "2",
+        "courseDesc": "狗狗学做饭",
+        "courseLabel": "做饭",
+        "courseCost": "20",
+        "courseImage": "FASLAJSDIUQWHDKJASHDKJASDASD",
+        "saleState": "1",
+        "courseOrder": "1000",
+        "trainerInfo": "范辉",
+        "level": "3",
+        "learningStep": "不知道",
+        "learningTimes": "11",
+        "JoinerCount": "1100",
+        "creatTime": "2018-06-15",
+        "creater": "范辉"
+      }
+    ],
+
+    // 系列课程
+    seriesLessons: [{
+        "id": "11",
+        "courseName": "狗狗学做饭",
+        "courseTitle": "做饭",
+        "courseLengthTime": "15",
+        "courseType": "2",
+        "courseDesc": "狗狗学做饭",
+        "courseLabel": "做饭",
+        "courseCost": "20",
+        "courseImage": "FASLAJSDIUQWHDKJASHDKJASDASD",
+        "saleState": "1",
+        "courseOrder": "1000",
+        "trainerInfo": "范辉",
+        "level": "3",
+        "learningStep": "不知道",
+        "learningTimes": "11",
+        "JoinerCount": "1100",
+        "creatTime": "2018-06-15",
+        "creater": "范辉"
+      },
+      {
+        "id": "12",
+        "courseName": "狗狗学做饭",
+        "courseTitle": "做饭",
+        "courseLengthTime": "15",
+        "courseType": "2",
+        "courseDesc": "狗狗学做饭",
+        "courseLabel": "做饭",
+        "courseCost": "20",
+        "courseImage": "FASLAJSDIUQWHDKJASHDKJASDASD",
+        "saleState": "1",
+        "courseOrder": "1000",
+        "trainerInfo": "范辉",
+        "level": "3",
+        "learningStep": "不知道",
+        "learningTimes": "11",
+        "JoinerCount": "1100",
+        "creatTime": "2018-06-15",
+        "creater": "范辉"
+      },
+      {
+        "id": "13",
+        "courseName": "狗狗学做饭",
+        "courseTitle": "做饭",
+        "courseLengthTime": "15",
+        "courseType": "2",
+        "courseDesc": "狗狗学做饭",
+        "courseLabel": "做饭",
+        "courseCost": "20",
+        "courseImage": "FASLAJSDIUQWHDKJASHDKJASDASD",
+        "saleState": "1",
+        "courseOrder": "1000",
+        "trainerInfo": "范辉",
+        "level": "3",
+        "learningStep": "不知道",
+        "learningTimes": "11",
+        "JoinerCount": "1100",
+        "creatTime": "2018-06-15",
+        "creater": "范辉"
+      }
+    ]
   },
   //事件处理函数
   bindViewLesson: function() {
@@ -17,35 +142,34 @@ Page({
       url: '../lesson/lesson'
     })
   },
-  bindViewDossier: function () {
+  bindViewDossier: function() {
     wx.navigateTo({
       url: '../dossier/dossier'
     })
   },
-  bindViewDrill: function (e) {
+  bindViewDrill: function(e) {
     if (e.currentTarget.dataset.checked) {
       wx.navigateTo({
         url: '../drill/drill'
       })
-    }
-    else {
+    } else {
       wx.navigateTo({
         url: '../lessonDetail/lessonDetail'
       })
     }
   },
-  bindViewLeslist: function () {
+  bindViewLeslist: function() {
     wx.navigateTo({
       url: '../lessonList/lessonList'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
