c5dba2856bd33d901cabd8e7892f3ab45f086615
“dingcheng” <dingcheng@qutoutiao.net>
2019-04-02 17:46:06 +0800

    保存本地代码

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -86,5 +86,34 @@ Page({
   //禁止滑动
   disMove: function () {
   },
-
+  confirmTopay: function() {
+    wx.request({
+      //我把文件夹名改为了wxpayapi,SERVER_PATH为服务器的域名
+      url: 'http://39.96.46.173/dpet-core/ipet/orderinfo/payment',
+      data: {
+        
+        orderId:'b6d009c2acd142bf8653b529c662f5a6',
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'x-auth-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Nzc2MzUyMDAwMDAsInVzZXJfaW5mbyI6IntcImlkXCI6XCIxMjM0NTZcIixcInByb3ZpZGVyS2V5XCI6XCJhc2Rhc2RcIixcInJvbGVfa2V5XCI6XCIxMTAyXCJ9In0.0a2_sMVAbtDGXyIotG7IiHBJRqZtgijqiRNrd-jBVk0',
+      },
+      success: function (res) {
+        console.log(res.data);
+        var data = res.data;
+        wx.requestPayment({
+          'timeStamp': data.timeStamp,
+          'nonceStr': data.nonceStr,
+          'package': data.package,
+          'signType': 'MD5',
+          'paySign': data.paySign,
+          'success': function (res) {
+            console.log("支付成功！")
+          },
+          'fail': function (res) {
+          }
+        })
+      }
+    })
+  }
 })
