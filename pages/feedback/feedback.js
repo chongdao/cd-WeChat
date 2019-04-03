40dcf812719706fe7e37d65f5232029dab02ae06
joe <2508387422@qq.com>
2019-04-03 20:07:18 +0800

    孙超提交

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -1,4 +1,7 @@
 // pages/feedback/feedback.js
+
+let sysApi = require('../../utils/api/system.js');
+
 Page({
 
   /**
@@ -12,59 +15,59 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  inputeidt: function (e) {
+  inputeidt: function(e) {
     let name = e.currentTarget.dataset.name;
     if (name == "fdoption") {
       this.setData({
@@ -77,12 +80,12 @@ Page({
       })
     }
   },
-  uploadimg: function () {
+  uploadimg: function() {
     var that = this;
     wx.showActionSheet({
       itemList: ["从相册中选择", "拍照"],
       itemColor: "#f7982a",
-      success: function (res) {
+      success: function(res) {
         if (!res.cancel) {
           if (res.tapIndex == 0) {
             that.chooseWxImage("album");
@@ -93,19 +96,19 @@ Page({
       }
     })
   },
-  chooseWxImage: function (type) {
+  chooseWxImage: function(type) {
     var that = this;
     wx.chooseImage({
       count: 1,
       sizeType: ["original", "compressed"],
       sourceType: [type],
-      success: function (res) {
+      success: function(res) {
         var addimg = res.tempFilePaths;
         that.upload(addimg[0]);
       }
     })
   },
-  upload: function (img) {
+  upload: function(img) {
     wx.showToast({
       title: '正在上传...',
       icon: 'loading',
@@ -122,17 +125,26 @@ Page({
       header: {
         "Content-Type": "multipart/form-data"
       },
-      success: function (res) {
-      },
-      fail: function (res) {
+      success: function(res) {},
+      fail: function(res) {
         wx.hideToast();
         wx.showModal({
           title: '错误提示',
           content: '上传图片失败',
           showCancel: false,
-          success: function (res) { }
+          success: function(res) {}
         })
       }
     });
   },
+
+  submit: function() {
+    sysApi.feedback(this.data.fdtel, this.data.fdoption).then(r => {
+      wx.showToast({
+        title: r.message,
+        icon: r.message === '操作成功' ? 'success' : 'error',
+        duration: 2000
+      })
+    })
+  }
 })
